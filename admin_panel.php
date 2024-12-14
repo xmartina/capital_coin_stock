cf02467bc1bf82c97b54cefe24d0b87309389862
xmartina <martinsprince126@gmail.com>
2024-12-14 03:50:12 +0100

    d

--- admin_panel.php
+++ admin_panel.php
@@ -193,6 +193,6 @@ $page_name = 'Admin Approve Deposit';
     // include_once __DIR__ . '/partials/footer.php'; 
     ?>
     <!-- Bootstrap JS Bundle (Includes Popper) -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
